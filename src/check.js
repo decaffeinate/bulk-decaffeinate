436df020ebf54e8cde71949a966400cd0dfb6274
Alan Pierce <alangpierce@gmail.com>
2016-09-24 12:26:10 -0700

    feat: record the successful files after the check command (#30)
    This makes it possible to do a "best effort" decaffeinate run that converts
    everything that can be converted automatically.

--- src/check.js
+++ src/check.js
@@ -16,17 +16,20 @@ async function printResults(results) {
   let errorResults = results.filter(r => r.error !== null);
   if (errorResults.length === 0) {
     console.log(`All checks succeeded! decaffeinate can convert all ${pluralize(results.length, 'file')}.`);
-    console.log('Run this command again with the convert command to convert the files to JavaScript.');
+    console.log('Run "bulk-decaffeinate convert" to convert the files to JavaScript.');
   } else {
     console.log(`${pluralize(errorResults.length, 'file')} failed to convert:`);
     for (let result of errorResults) {
       console.log(result.path);
     }
+    let successPaths = results.filter(r => r.error === null).map(r => r.path);
     console.log();
     await writeFile('decaffeinate-errors.log', getVerboseErrors(results));
     await writeFile('decaffeinate-results.json', JSON.stringify(results, null, 2));
+    await writeFile('decaffeinate-successful-files.txt', successPaths.join('\n'));
     console.log('Wrote decaffeinate-errors.log and decaffeinate-results.json with more detailed info.');
-    console.log('To open failures in the online repl, run "bulk-decaffeinate view-errors"');
+    console.log('To open failures in the online repl, run "bulk-decaffeinate view-errors".');
+    console.log('To convert the successful files, run "bulk-decaffeinate convert -p decaffeinate-successful-files.txt".');
   }
 }
 
