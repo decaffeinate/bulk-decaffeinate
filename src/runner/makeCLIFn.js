dbcd655035df7a06f886b2261ac0bcc098090fe8
Alan Pierce <alangpierce@gmail.com>
2016-12-19 12:19:35 -0800

    fix: increase buffer size to 5MB when running CLI commands
    Some decaffeinate runs had more than 200KB of stdout, which gave an error
    message.

--- src/runner/makeCLIFn.js
+++ src/runner/makeCLIFn.js
@@ -3,7 +3,7 @@ import { exec } from 'mz/child_process';
 export default function makeCLIFn(commandByPath) {
   return async function(path) {
     try {
-      await exec(commandByPath(path));
+      await exec(commandByPath(path), {maxBuffer: 5 * 1024 * 1024});
       return {path, error: null};
     } catch (e) {
       return {path, error: e.message};
